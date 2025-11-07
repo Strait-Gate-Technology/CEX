22c11dda5dceb772c6abd639504fbd7bcd71280e
amellalalex <github@alexamellal.com>
2025-11-07 00:29:19 -0400

    Longer message status
    All in all, I think NUM could use some work. Notably in how it is initialized; I think the _128bit specifying is a bit lopsided and doesn't follow the constructor mantra of specifying the initial value. It could be better to do something like NUM_128(value) to work with MULTIPLE NUM types which all fit into NUM. I quite like that idea actually. Regardless, NUM still needs some work in being useful. How will addition work? How will the value be represented from the outside even if *super* long division multiplication addition works? Perhaps it could be presented as a string. Should this be a char* or a STR? Will it need to nest a STR within itself? These are all questions that I'm discovering

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main()
 {
     STR* name = STR("Alex");
     STR* copy = STRCPY(name);
-    NUM* age = NUM(24);
+    NUM* age = NUM(_128bit);
 
     printf("Hello %s\n", name->str);
     printf("The copy of your name is %s\n", copy->str);
