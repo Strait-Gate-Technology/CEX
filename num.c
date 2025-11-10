93580540bd255001a8589ef833d18802eefb131d
amellalalex <github@alexamellal.com>
2025-11-10 11:13:37 -0400

    Explicitly initializing NUM OBJ with sizeof(int)
    This is better than the sizeof(i) where 'i' was the variable parameter to the maker func of NUM

--- num.c
+++ num.c
@@ -8,7 +8,7 @@ NUM* cex_make_num(int i)
 {
     NUM* new_num;
     new_num = malloc(sizeof(NUM));
-    new_num->self = OBJ(sizeof(i));
+    new_num->self = OBJ(sizeof(int));
     new_num->nums = new_num->self->dat;
     new_num->nums[0] = i;
     new_num->len = 1;
