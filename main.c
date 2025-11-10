e365fdd75e84ebc0a789844463c17c52ca715989
amellalalex <github@alexamellal.com>
2025-11-10 11:28:42 -0400

    Basic working example of ADD() in main()

--- main.c
+++ main.c
@@ -7,13 +7,21 @@
 
 int main()
 {
+    int i;
+
     STR* name = STR("Alex");
     STR* copy = COPY(name);
     NUM* age = NUM(24);
+    NUM* inc = NUM(1);
 
     printf("Hello %s\n", name->str);
     printf("The copy of your name is %s\n", copy->str);
     printf("The value of your age is %d\n", age->nums[0]);
+
+    for (i = 0; i < 5; i++) {
+        ADD(age, inc);
+        printf("ADD(age, inc), age = %d\n", age->nums[0]);
+    }
 
     FREE(name);
     FREE(copy);
