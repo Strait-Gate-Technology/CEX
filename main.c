44881682760c09783261bdd99a5fc01ad0872242
amellalalex <github@alexamellal.com>
2025-11-10 12:09:42 -0400

    Modified CMakeLists.txt to be DEBUG friendly, same in main.c

--- main.c
+++ main.c
@@ -7,12 +7,43 @@
 
 int main()
 {
+    /* Testing resize */
+    int i, c;
+    NUM* n1 = NUM(1);
+    NUM* n2 = NUM(~(1u));
+
+    printf("n1->nums[0] = %u\n", n1->nums[0]);
+    printf("n1->len = %lu\n", n1->len);
+
+    printf("n2->nums[0] = %u\n", n2->nums[0]);
+    printf("n2->len = %lu\n", n2->len);
+
+    /* Add slowly */
+    while (c != 'q') {
+        ADD(n2, n1);
+
+        printf("n1->nums[0] = %u\n", n1->nums[0]);
+        printf("n1->len = %lu\n", n1->len);
+
+        printf("n2->nums[0] = %u\n", n2->nums[0]);
+        printf("n2->len = %lu\n", n2->len);
+
+        printf("Do it again? ([ENTER] to continue or 'q' to quit): ");
+        c = getchar();
+    }
+
+    FREE(n1);
+    FREE(n2);
+}
+
+int main_1()
+{
     int i;
 
     STR* name = STR("Alex");
     STR* copy = COPY(name);
     NUM* age = NUM(24);
-    NUM* inc = NUM(1);
+    NUM* inc = NUM((1u << (sizeof(int) * 8 - 1)) - 1);
 
     printf("Hello %s\n", name->str);
     printf("The copy of your name is %s\n", copy->str);
@@ -20,7 +51,7 @@ int main()
 
     for (i = 0; i < 5; i++) {
         ADD(age, inc);
-        printf("ADD(age, inc), age = %d\n", age->nums[0]);
+        printf("ADD(age, inc), age = %u\n", age->nums[0]);
     }
 
     FREE(name);
